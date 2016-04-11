7e756a35ce69fa059bae486a3aa116efddeff110
DK KD <DK KD>
2016-04-12 00:50:28 +0200

    T1 B3

--- ConsoleApplication2/Program.cs
+++ ConsoleApplication2/Program.cs
@@ -11,6 +11,7 @@ namespace ConsoleApplication2
         static void Main(string[] args)
         {
             Console.WriteLine("TEST APL! T1");
+            Console.WriteLine("TEST T1 B3 TEST");
 
             // Just to keep the console window open in debug mode.
             Console.WriteLine("Press any key to exit.");
